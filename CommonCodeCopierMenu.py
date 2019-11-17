6c52e29faed061aaa0789cafb90882e693ff578e
Eric McDaniel <McDanielES@gmail.com>
2019-11-16 20:38:34 -0600

    Center title name and color pair

--- CommonCodeCopierMenu.py
+++ CommonCodeCopierMenu.py
@@ -10,10 +10,19 @@ import curses
   
   
 def init(screen):
+  curses.start_color()
+  curses.use_default_colors()
+  curses.init_pair(1, curses.COLOR_RED, -1)
+
   screen.border(0)
+  screen.addstr(1, 5, "Welcome to the")
+  screen.addstr(1, 20, "Common Code Copier", curses.color_pair(1))
+  screen.addstr(2, 6, "by Eric McDaniel, November 2019")
+  # screen.addstr(0,0, "RED ALERT!", curses.color_pair(1))
   
   box1 = curses.newwin(30, 40, 3, 1)
-  box1.box()    
+  box1.box()
+  
 
   screen.refresh()
   box1.refresh()
