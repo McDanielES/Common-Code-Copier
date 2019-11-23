34af49109699f77b8def08c37b00133338ca20c6
Eric McDaniel <McDanielES@gmail.com>
2019-11-23 00:56:25 -0600

    Merged the former test.py

--- CommonCodeCopierMenu.py
+++ CommonCodeCopierMenu.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import sys
 import curses
+import pyperclip
+
+window_height = 40
+window_width  = 30
+
 
 def init(screen):
   curses.start_color()
@@ -14,18 +19,40 @@ def init(screen):
   screen.attroff(curses.A_BOLD)
   screen.addstr(2, 6, "by Eric McDaniel, November 2019")
   
-  box1 = curses.newwin(30, 40, 3, 1)
+  box1 = curses.newwin(window_width, window_height, 3, 1)
   box1.box()
   screen.refresh()
   box1.refresh()
 
   screen.getch()
   
+def readFile():
+  clipboard = open("code-options.txt", "r")
+  print("Please choose one option from the following")
+  count = 1
+  codeList = []
+  for line in clipboard:
+    codeList.append(line)
+    print("  {}. {}".format(count, line), end = "")
+    count = count + 1
+  print("\nYour selection: ", end = "")
+  selection = input()
+  print("You chose:")
+  print(codeList[int(selection) - 1])
+
+  pyperclip.copy(codeList[int(selection) - 1])
+  
+# Implement
+def getWindowText():
+  Text = []
+  Text.append("Please choose one option from the following")
+  
 
 def main():
+  # readFile()
   init(curses.initscr())
-  from subprocess import call
-  call("exit 0", shell=True)
+  # from subprocess import call
+  # call("exit 0", shell=True)
 
 # Start Program with main
 if __name__ == '__main__':
@@ -33,5 +60,3 @@ if __name__ == '__main__':
     main()
   except KeyboardInterrupt:
     sys.exit(1)
-  except Exception:
-    sys.exit(2)
