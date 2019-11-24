a7d6bf5bf2575d673d757febdb1f473358a8824b
Eric McDaniel <McDanielES@gmail.com>
2019-11-24 03:57:22 -0600

    Complete draw menu and basic concept

--- CommonCodeCopierMenu.py
+++ CommonCodeCopierMenu.py
@@ -3,14 +3,18 @@ import sys
 import curses
 import pyperclip
 
-window_height = 40
-window_width  = 30
+box_width  = 40
+box_height = 30
+input_box_height = 6
+
+# screen.move(50, 50)
 
 
 def draw_window(screen):
   curses.start_color()
   curses.use_default_colors()
   curses.init_pair(1, curses.COLOR_RED, -1)
+  curses.init_pair(2, curses.COLOR_CYAN, -1)
 
   screen.border(0)
   screen.addstr(1, 5, "Welcome to the")
@@ -20,13 +24,13 @@ def draw_window(screen):
   screen.addstr(2, 6, "by Eric McDaniel, November 2019")
   
   
-  box1 = curses.newwin(window_width, window_height, 3, 1)
+  box1 = curses.newwin(box_height, box_width, 3, 1)
   box1.box()
   screen.refresh()
   box1.refresh()
   screen.addstr(4, 6, "Please choose one option from")
   screen.addstr(5, 9, "the following selection")
-  for x in range(2, window_height):
+  for x in range(2, box_width):
     screen.addch(6, x, curses.ACS_HLINE)
   
   # Read the textfile
@@ -35,31 +39,35 @@ def draw_window(screen):
   for line in Code:
     screen.addstr(row, 3, "{}. {}".format((row - 6), line.strip()))
     row = row + 1
+    
+  # Draw User selection box
+  input_box = curses.newwin(input_box_height, box_width - 2, box_height - input_box_height + 2, 2)
+  input_box.box()
+  input_box.refresh()
+  screen.addstr(box_height - 3, 6, "User Selection: ", curses.color_pair(2))
 
-  screen.getch()
+
+  # Throw an exception if user input is improper
+  selection = screen.getch()
+  if (selection < 48 or selection > 48 + len(Code)):
+    raise ValueError
   
+  # Ascii value conversion of char 1 to decimal 1
+  selection = selection - 49
+  pyperclip.copy(Code[int(selection)])
+
+  
+  screen.getch()  
+
 def readFile():
   clipboard = open("code-options.txt", "r")
   count = 1
   codeList = []
   for line in clipboard:
     codeList.append(line)
-    count = count + 1
-    
+    count = count + 1    
   clipboard.close()
-  return codeList
-    
-  # print("\nYour selection: ", end = "")
-  # selection = input()
-  # print("You chose:")
-  # print(codeList[int(selection) - 1])
-
-  # pyperclip.copy(codeList[int(selection) - 1])
-  
-# Implement
-def getWindowText(file):
-  Text = []
-  
+  return codeList  
 
 def main():
   draw_window(curses.initscr())
@@ -67,9 +75,10 @@ def main():
   # from subprocess import call
   # call("exit 0", shell=True)
 
-# Start Program with main
 if __name__ == '__main__':
   try:
     main()
   except KeyboardInterrupt:
     sys.exit(1)
+  except ValueError:
+    sys.exit(2)
