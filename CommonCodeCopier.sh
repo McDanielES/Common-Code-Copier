13a92f0b5d1184e8dbbfa8099ffeda2148edb762
Eric McDaniel <McDanielES@gmail.com>
2019-11-30 02:15:53 -0600

    Modify absolute path for all users cloning this repo

--- CommonCodeCopier.sh
+++ CommonCodeCopier.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-termite -e 'python3 /home/eric/Documents/projects/CommonCodeCopier/CommonCodeCopierMenu.py' 2>/dev/null
+termite -e 'python3 <path>/<to>/<directory>/CommonCodeCopierMenu.py' 2>/dev/null
